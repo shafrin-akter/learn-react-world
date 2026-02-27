fd804e25f69884c03b565f65028a4f84d253c842
shafrinakterr <shafrinakterr@gmail.com>
2026-02-27 08:43:21 +0600

    data fetching and looping

--- src/componects/countries/Countri/Countri.jsx
+++ src/componects/countries/Countri/Countri.jsx
@@ -1,5 +1,62 @@
-const Countri = () => {
-  return <div></div>;
+// {
+//     "name": {
+//         "common": "Jamaica",
+//         "official": "Jamaica"
+//     },
+//     "ccn3": {
+//         "ccn3": "388"
+//     },
+//     "currencies": {
+//         "currencies": {
+//             "JMD": {
+//                 "name": "Jamaican dollar",
+//                 "symbol": "$"
+//             }
+//         }
+//     },
+//     "capital": {
+//         "capital": [
+//             "Kingston"
+//         ]
+//     },
+//     "region": {
+//         "region": "Americas"
+//     },
+//     "languages": {
+//         "languages": {
+//             "eng": "English",
+//             "jam": "Jamaican Patois"
+//         }
+//     },
+//     "area": {
+//         "area": 10991
+//     },
+//     "cca3": {
+//         "cca3": "JAM"
+//     },
+//     "population": {
+//         "population": 2961161
+//     },
+//     "continents": {
+//         "continents": [
+//             "North America"
+//         ]
+//     },
+//     "flags": {
+//         "flags": {
+//             "png": "https://flagcdn.com/w320/jm.png",
+//             "svg": "https://flagcdn.com/jm.svg",
+//             "alt": "The flag of Jamaica is divided by a gold diagonal cross into four alternating triangular areas of green at the top and bottom, and black on the hoist and fly sides"
+//         }
+//     }
+// }
+const Countri = ({ countrie }) => {
+  console.log(countrie);
+  return (
+    <div>
+      <h1>{countrie.name.common}</h1>
+    </div>
+  );
 };
 
 export default Countri;
--- src/componects/countries/Cuntries.jsx
+++ src/componects/countries/Cuntries.jsx
@@ -1,11 +1,15 @@
 import { use } from "react";
+import Countri from "./Countri/Countri";
 const Cuntries = ({ coutriesPromise }) => {
   const countriesDeata = use(coutriesPromise);
   const countries = countriesDeata.countries;
-  console.log(countriesDeata);
+  //   console.log(countriesDeata);
   return (
     <div>
       <h1>Countries: {countries.length}</h1>
+      {countries.map((countrie) => (
+        <Countri key={countrie.id} countrie={countrie}></Countri>
+      ))}
     </div>
   );
 };
