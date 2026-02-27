71287a908683357a6628adf52bbc13d332954fa5
shafrinakterr <shafrinakterr@gmail.com>
2026-02-27 17:52:59 +0600

    toggle state and visited countrie count

--- src/componects/countries/Countri/Countri.jsx
+++ src/componects/countries/Countri/Countri.jsx
@@ -1,12 +1,36 @@
-const Countri = ({ countrie }) => {
+import { useState } from "react";
+import "./countri.css";
+const Countri = ({ countrie, count, setCount }) => {
+  console.log(countrie);
   const { name, flags, capital, continents, languages } = countrie;
+  const [visited, setVisited] = useState(false);
+
+  // Visited Button
+  const handleVisited = () => {
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // setVisited(visited ? false : true);
+
+    setVisited(!visited);
+    if (!count) {
+      setCount((prev) => prev + 1);
+    } else {
+      setCount((prev) => prev - 1);
+    }
+  };
   return (
-    <div>
+    <div className={`countri  ${visited && "countri-visited"}`}>
       <img src={flags?.flags?.png} alt={flags?.flags?.alt} />
       <h3>Name: {name.common}</h3>
       <h3>Capital: {capital.capital}</h3>
       {languages?.languages?.eng && <p>Language: {languages.languages.eng}</p>}
       <p>Continents: {continents.continents}</p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
--- src/componects/countries/Cuntries.jsx
+++ src/componects/countries/Cuntries.jsx
@@ -1,15 +1,26 @@
-import { use } from "react";
+import { use, useState } from "react";
 import Countri from "./Countri/Countri";
+import "./Countries.css";
 const Cuntries = ({ coutriesPromise }) => {
   const countriesDeata = use(coutriesPromise);
   const countries = countriesDeata.countries;
+  const [count, setCount] = useState(0);
+
   //   console.log(countriesDeata);
   return (
     <div>
       <h1>Countries: {countries.length}</h1>
-      {countries.map((countrie) => (
-        <Countri key={countrie.cca3.cca3} countrie={countrie}></Countri>
-      ))}
+      <h2>Visited: {count}</h2>
+      <div className="card">
+        {countries.map((countrie) => (
+          <Countri
+            setCount={setCount}
+            count={count}
+            key={countrie.cca3.cca3}
+            countrie={countrie}
+          ></Countri>
+        ))}
+      </div>
     </div>
   );
 };
